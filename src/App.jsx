ea6f5ee679321a84bbe7f7f386e17d9e123bb685
anuj upadhyay <anuju760@gmail.com>
2023-09-29 17:19:05 +0530

    commited the list search project

---
+++ src/App.jsx
@@ -0,0 +1,142 @@
+
+
+import { useState } from 'react';
+import './App.css'
+
+function ProductCategoryRow({category}){
+
+  return(
+    <tr>
+      <th colSpan='2'>{category}</th>
+    </tr>
+    
+  )
+  
+}
+
+function ProductRow({product}){
+  const name= product.stocked ? product.name:<span style={{color:'red'}}>{product.name}</span>;
+return(
+  <tr>
+    <td>{name}</td>
+    <td>{product.price}</td>
+
+  </tr>
+)
+}
+
+function ProductTable({products,filterText,inStockOnly}){
+  const rows = []
+  let lastCategory =null;
+  products.forEach((product)=>{
+    if (
+      product.name.toLowerCase().indexOf(
+        filterText.toLowerCase()
+      ) === -1
+    ) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+
+    if(product.category !== lastCategory){
+      rows.push(
+        <ProductCategoryRow 
+        category={product.category}
+        key={product.category}
+        />
+      )
+    }
+    rows.push(
+      <ProductRow 
+      product={product}
+      key={product.name}
+      
+      />
+    );
+    lastCategory = product.category;
+  })
+  return(
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar({filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange}){
+return(
+  <form >
+    <input type="text" placeholder='Search...' value={filterText} onChange={(e)=>onFilterTextChange (e.target.value)}/>
+    <label >
+      <input type="checkbox" checked={inStockOnly}
+      onChange={(e)=>onInStockOnlyChange(e.target.value)}/>
+      {''}
+      Only show products in stock
+    </label>
+  </form>
+)
+}
+
+function FilterableProductTable({products}){
+  const [filterText,setFilterText]=useState('')
+  const [inStockOnly,setInStockOnly]=useState(false);
+return(
+  <div>
+    <SearchBar
+    filterText={filterText}
+    inStockOnly={inStockOnly}
+    onFilterTextChange={setFilterText}
+    onInStockOnlyChange={setInStockOnly}
+    />
+    <ProductTable 
+    products={products}
+    filterText={filterText}
+    inStockOnly={inStockOnly}
+    
+    />
+
+  </div>
+)
+}
+
+const PRODUCTS=[
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Watermelon"},
+  {category: "Fruits", price: "$1", stocked: true, name: "PineApple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Kiwi"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Oranges"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Guava"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Pears"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Lichi"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"},
+  {category: "Vegetables", price: "$1", stocked: false, name: "Guards"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Corriender"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Potato"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Beans"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Tomato"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Cauliflower"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Waterguard"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Greenchillis"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Jackfruit"},
+];
+ export default function App() {
+  
+
+  return <FilterableProductTable products={PRODUCTS}/>
+}
+
+
